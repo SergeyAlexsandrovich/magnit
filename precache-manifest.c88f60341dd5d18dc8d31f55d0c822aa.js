929f320020e4c524f323c3f81da4b8881f7de57a
David Arutiunian <arutunjan666@mail.com>
2019-06-11 21:09:00 +0300

    Updates

---
+++ precache-manifest.c88f60341dd5d18dc8d31f55d0c822aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce30442d33c77d47f0c58badedf7f97b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b10d5a6ed77df2a0e0c",
+    "url": "/static/js/0.8ec4ee97.chunk.js"
+  },
+  {
+    "revision": "3d3094ca0ac31d05137f",
+    "url": "/static/js/3.74bb706e.chunk.js"
+  },
+  {
+    "revision": "671f438c7b5ca698d20c",
+    "url": "/static/js/4.8d293c68.chunk.js"
+  },
+  {
+    "revision": "404982d80c1a0966ac0f",
+    "url": "/static/js/5.7d2452fd.chunk.js"
+  },
+  {
+    "revision": "65be19498f13b55a3377",
+    "url": "/static/js/6.ec5ff627.chunk.js"
+  },
+  {
+    "revision": "e7064cc04a8ddc1ef675",
+    "url": "/static/js/7.7f10a225.chunk.js"
+  },
+  {
+    "revision": "031b63cf90894cb3516c",
+    "url": "/static/js/main.ded815dc.chunk.js"
+  },
+  {
+    "revision": "acfb2f005d350b7440ba",
+    "url": "/static/js/runtime~main.09267c76.js"
+  }
+]);
